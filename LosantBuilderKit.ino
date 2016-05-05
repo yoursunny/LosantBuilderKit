83711dd4c7a096357c0b460f3ceb2f3d517ce4ea
Junxiao Shi <git@mail1.yoursunny.com>
2016-05-05 01:05:38 -0700

    sketch with button, led, tempC, ping-pong

---
+++ LosantBuilderKit.ino
@@ -0,0 +1,199 @@
+#include <ESP8266WiFi.h>
+#include <Losant.h>
+#include "credentials.h"
+
+const int LOOP_INTERVAL = 100;
+
+WiFiClientSecure wifiClient;
+LosantDevice device(LOSANT_DEVICE_ID);
+
+bool connect()
+{
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to WiFi ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.print("WiFi connected, local IP is ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println();
+  Serial.println("Connecting to Losant");
+  device.connectSecure(wifiClient, LOSANT_ACCESS_KEY, LOSANT_ACCESS_SECRET);
+  while (!device.connected()) {
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println();
+      Serial.println("WiFi connection lost");
+      return false;
+    }
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Connected to Losant");
+  Serial.println();
+  Serial.println();
+
+  return true;
+}
+
+void ensureConnected()
+{
+  bool needReconnect = false;
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("WiFi disconnected");
+    needReconnect = true;
+  }
+  if (!device.connected()) {
+    Serial.println("Losant disconnected");
+    needReconnect = true;
+  }
+  while (needReconnect) {
+    needReconnect = !connect();
+  }
+}
+
+const int BUTTON_PIN = 14;
+int buttonState = 0;
+
+void buttonPressed()
+{
+  Serial.println("Button pressed.");
+  Serial.println();
+
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["act"] = "button";
+  device.sendState(root);
+}
+
+void readButton()
+{
+  int currentRead = digitalRead(BUTTON_PIN);
+  if (currentRead != buttonState) {
+    buttonState = currentRead;
+    if (buttonState) {
+      buttonPressed();
+    }
+  }
+}
+
+const int LED_PIN = 12;
+bool ledState = false;
+
+void toggleLed()
+{
+  Serial.println("Toggling LED");
+  ledState = !ledState;
+  digitalWrite(LED_PIN, ledState ? HIGH : LOW);
+}
+
+const int TEMP_REPORT_INTERVAL = 15000;
+int tempReportDue = TEMP_REPORT_INTERVAL;
+int tempSum = 0;
+int tempCount = 0;
+
+void reportTemp(double tempC)
+{
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["tempC"] = tempC;
+  device.sendState(root);
+}
+
+void readTemp()
+{
+  tempSum += analogRead(A0);
+  ++tempCount;
+  tempReportDue -= LOOP_INTERVAL;
+  if (tempReportDue <= 0) {
+    double raw = (double)tempSum / (double)tempCount;
+    double tempC = ((raw / 1024.0 * 2.0) - 0.57) * 100.0;
+    Serial.print("Reporting temperature: ");
+    Serial.print(tempC);
+    Serial.println(" C");
+    reportTemp(tempC);
+    tempSum = tempCount = 0;
+    tempReportDue = TEMP_REPORT_INTERVAL;
+  }
+}
+
+const int PING_INTERVAL = 10000;
+const int PONG_MISS_THRESHOLD = 6;
+int pingDue = TEMP_REPORT_INTERVAL;
+bool hasPong = false;
+int pongMissed = 0;
+
+void ping()
+{
+  pingDue -= LOOP_INTERVAL;
+  if (pingDue > 0) {
+    return;
+  }
+  pingDue = PING_INTERVAL;
+
+  Serial.println("Sending ping");
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["act"] = "ping";
+  device.sendState(root);
+
+  if (!hasPong) {
+    ++pongMissed;
+    Serial.print("Pong missed: ");
+    Serial.println(pongMissed);
+    if (pongMissed >= PONG_MISS_THRESHOLD) {
+      pongMissed = 0;
+      Serial.println("Too many missed pongs, disconnecting");
+      device.disconnect();
+      return;
+    }
+  }
+}
+
+void witnessPong()
+{
+  hasPong = true;
+}
+
+void handleCommand(LosantCommand* cmd) {
+  Serial.print("Command received: ");
+  Serial.println(cmd->name);
+
+  if (strcmp(cmd->name, "toggle") == 0) {
+    toggleLed();
+  }
+  if (strcmp(cmd->name, "pong") == 0) {
+    witnessPong();
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(2000);
+
+  Serial.println();
+
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  device.onCommand(&handleCommand);
+}
+
+void loop()
+{
+  ensureConnected();
+  device.loop();
+
+  readButton();
+  readTemp();
+  ping();
+  delay(LOOP_INTERVAL);
+}
